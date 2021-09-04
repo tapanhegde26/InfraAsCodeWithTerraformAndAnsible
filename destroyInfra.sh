36b09831b1a24361dff6c6b5f48873958aba24c8
c5281159 <tapan.kumar.hegde@sap.com>
2021-09-04 23:50:01 +0530

    default region paramater added in destroyInfra script

--- destroyInfra.sh
+++ destroyInfra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export AWS_DEFAULT_REGION=us-east-1
 cd terraform
 terraform destroy -var "pvt_key=~/.ssh/id_rsa" -var "pub_key=~/.ssh/id_rsa.pub" -auto-approve
 echo "Infra is destroyed"
