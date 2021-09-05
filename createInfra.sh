7dfd7445a0bc9ac524fa3c21686d2fee4228081a
c5281159 <tapan.kumar.hegde@sap.com>
2021-09-05 16:38:32 +0530

    changes made to script

--- createInfra.sh
+++ createInfra.sh
@@ -4,9 +4,8 @@ cd terraform
 terraform init
 terraform apply -var "pvt_key=~/.ssh/id_rsa" -var "pub_key=~/.ssh/id_rsa.pub" -auto-approve
 export terraformApplyStatus=$?
-if [ !["$terraformApplyStatus"]]
-	then
-		echo "Infrastructure created successfully using terraform apply command!!"
-	else
-		echo "Infrastructure creation failed. Check terraform apply logs!!"
+if [[ "$terraformApplyStatus" -ne 0 ]]; then
+    echo "Infrastructure creation failed. Check terraform apply logs!!"
+else
+    echo "Infrastructure created successfully using terraform apply command!!"
 fi
--- destroyInfra.sh
+++ destroyInfra.sh
@@ -3,9 +3,8 @@ export AWS_DEFAULT_REGION=us-east-1
 cd terraform
 terraform destroy -var "pvt_key=~/.ssh/id_rsa" -var "pub_key=~/.ssh/id_rsa.pub" -auto-approve
 echo terraformDestroyStatus=$?
-if [! ["$terraformDestroyStatus]]
-	then
-		echo "Infrastructure destroyed successfully with terraform destroy command!!"
-	else
-		echo "Infrastructure destruction failed. Please check terraform logs!!
+if [[ "$terraformDestroyStatus" -ne 0 ]]; then
+	echo "Infrastructure destruction failed. Please check terraform logs!!"
+else
+	echo "Infrastructure destroyed successfully with terraform destroy command!!"
 fi
