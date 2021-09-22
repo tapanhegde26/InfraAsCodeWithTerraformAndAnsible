c685fed849b4b89696a2bd3115b3858e11fd618d
Ubuntu <ubuntu@ip-172-31-53-56.ec2.internal>
2021-09-22 18:26:01 +0000

    changes made to install_mediawiki script file

--- ansible/install_mediawiki.sh
+++ ansible/install_mediawiki.sh
@@ -31,12 +31,15 @@ if [[ $operatingSystem == *"Ubuntu"* ]]; then
 elif [[ $operatingSystem == *"Red Hat"* ]]; then
   echo "****** Linux Operating system is 'Redhat' ******"
   sudo yum install wget -y
-  sudo wget https://releases.wikimedia.org/mediawiki/1.36/mediawiki-1.36.1.tar.gz
-  sudo tar -zxpvf *.tar.gz
-  sudo mkdir /var/www/html/mediawiki
-  sudo mv mediawiki-* /var/www/html/mediawiki/
-  sudo chown -R apache:apache /var/www/html/mediawiki/
-  sudo chmod 755 /var/www/html/mediawiki/
+  cd /tmp/
+  wget https://releases.wikimedia.org/mediawiki/1.36/mediawiki-1.36.1.tar.gz
+  tar -xvzf /tmp/mediawiki-*.tar.gz
+  sudo mkdir /var/lib/mediawiki
+  sudo mv mediawiki-*/* /var/lib/mediawiki
+  sudo ln -s /var/lib/mediawiki /var/www/html/mediawiki
+  #sudo mv mediawiki-* /var/www/html/mediawiki/
+  #sudo chown -R apache:apache /var/www/html/mediawiki/
+  #sudo chmod 755 /var/www/html/mediawiki/
   sudo restorecon -FR /var/www/html/mediawiki/
   sudo dnf install php-intl -y
 elif [[ $operatingSystem == *"kali"* ]]; then
